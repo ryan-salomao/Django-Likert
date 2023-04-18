e2f0247e8b50ac23e4e9a4a29e43c4ad5282064b
Ryan Salomão <ryansalomao@gmail.com>
2023-04-18 02:49:40 -0300

    update template view with likert scale

--- app/models.py
+++ app/models.py
@@ -2,6 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Funcionario(models.Model):
-    atendimento = models.FloatField(max_length=3)
-    pontualidade = models.FloatField(max_length=3)
-    qualidade = models.FloatField(max_length=3)
+    NOTAS = [
+        (-2, '😡'),
+        (-1, '🙁'),
+        (0, '😐'),
+        (1, '🙂'),
+        (2, '😀')
+    ]
+    atendimento = models.IntegerField(choices=NOTAS)
+    pontualidade = models.IntegerField(choices=NOTAS)
+    qualidade = models.IntegerField(choices=NOTAS)
+
+    def media(self):
+        return (self.atendimento + self.pontualidade + self.qualidade) / 3
+
+    def template(self, atributo):
+        return atributo(choices=self.NOTAS)
--- app/views.py
+++ app/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app.forms import FuncionarioForm
+from app.models import Funcionario
 
 def home(request):
-    return render(request, 'index.html')
+    data = {}
+    data['db'] = Funcionario.objects.all()
+    return render(request, 'index.html', data)
 
 def form(request):
     data = {}
     data['form'] = FuncionarioForm()
     return render(request, 'form.html', data)
+
+def create(request):
+    form = FuncionarioForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
